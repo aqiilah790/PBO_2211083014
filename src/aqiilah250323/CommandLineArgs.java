7662f497b18146921f4f45178acd6b02e8f8b980
muhammad aqiilah paligie <127171345+aqiilah790@users.noreply.github.com>
2023-03-25 11:25:25 +0700

    update

---
+++ src/aqiilah250323/CommandLineArgs.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aqiilah250323;
+
+/**
+ *
+ * @author User
+ */
+public class CommandLineArgs{
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
+
+
