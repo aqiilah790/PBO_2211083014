85af4a631a8193adbfac7b06a74c2f20dbcf5772
muhammad aqiilah paligie <127171345+aqiilah790@users.noreply.github.com>
2023-04-01 21:57:25 +0700

    update

---
+++ src/aqiilah010423/AddressBook.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aqiilah010423;
+
+/**
+ *
+ * @author User
+ */
+public class AddressBook {
+    public static void main(String[] args) {
+        // contoh penggunaan class BukuAlamat
+        AddressBookEntry[] daftarAlamat = new AddressBookEntry[100];
+        
+        // memasukkan data
+        daftarAlamat[0] = new AddressBookEntry("John Doe", "Jl. Merdeka No. 10", "081234567890", "johndoe@gmail.com");
+        daftarAlamat[1] = new AddressBookEntry("Muhammad", "Jl. Sudirman No. 15", "081234567891", "Muhammad@yahoo.com");
+        
+        // menghapus data
+        daftarAlamat[1] = null;
+        
+        // menampilkan seluruh data
+        for (AddressBookEntry alamat : daftarAlamat) {
+            if (alamat != null) {
+                System.out.println("Nama: " + alamat.getName());
+                System.out.println("Alamat: " + alamat.getAddress());
+                System.out.println("Nomor Telepon: " + alamat.getPhoneNumber());
+                System.out.println("Alamat Email: " + alamat.getEmail());
+                System.out.println();
+            }
+        }
+        
+        // update data
+        daftarAlamat[0].setPhoneNumber("081234567891");
+        System.out.println("Nomor Telepon " + daftarAlamat[0].getName() + " telah diupdate");
+        System.out.println("Nomor Telepon terbaru: " + daftarAlamat[0].getPhoneNumber());
+    }
+
+}
+
