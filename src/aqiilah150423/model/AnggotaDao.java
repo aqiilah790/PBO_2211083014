cb1ed224dc6b96707ea09c99e90ee4d6e9ebefce
muhammad aqiilah paligie <127171345+aqiilah790@users.noreply.github.com>
2023-04-15 21:29:33 +0700

    update

---
+++ src/aqiilah150423/model/AnggotaDao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package aqiilah.model;
+import java.util.List;
+/**
+ *
+ * @author User
+ */
+public interface AnggotaDao {
+    void save(Anggota anggota);
+    void update(int index, Anggota anggota);
+    void delete(int index);
+    Anggota getAnggota(int index);
+    List<Anggota> getAll();
+}
